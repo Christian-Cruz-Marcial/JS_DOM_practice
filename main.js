0639b8bd871d848815824e39e01fa54ebf6b2532
Christian Cruz <c.cruzmarcial2001@gmail.com>
2023-10-05 12:20:26 -0500

    JS DOM Excercises

---
+++ main.js
@@ -0,0 +1,6 @@
+//change div backgroud color
+document.querySelector('#myDiv').style.backgroundColor = "lightblue";
+
+//log inner text of p element
+let text = document.querySelectorAll('p').innerText;
+console.log(text);
